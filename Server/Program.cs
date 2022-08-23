911fe8ba1ff5bfa5d5e051a7316c961dece0767c
payman nosraty <paymannosraty@gmail.com>
2022-08-23 15:45:55 +0430

    Add Error folder and pages

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder =
+	WebApplication.CreateBuilder();
 
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
+var app =
+	builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+else
+{
+	app.UseExceptionHandler("/Errors/Error");
+
+	// The default HSTS value is 30 days.
+	// You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapRazorPages();
 
